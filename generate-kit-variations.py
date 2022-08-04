d280237be5498bb1878b4baddbef85df50665288
Bob/AT <BobAT@gmx.at>
2022-08-05 01:22:33 +0200

    Mix up Narco clothing

--- generate-kit-variations.py
+++ generate-kit-variations.py
@@ -3,33 +3,52 @@
 # Script for generating some shirt/pant color variations
 # Reads *.json and writes *{1,2,4,5,6,7,8}.kit files
 
-VARIATIONS=[
+VARIATIONS = [
     # 1
-    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Green')],
+    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Green'),
+     ('HeadGear', 'HeadGear:BP_Hat_BaseBall', 'OD'),
+     ('Belt', 'Belt:BP_Battlebelt_TF', 'RangerGreen')],
     # 2
-    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Red')],
+    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Red'),
+     ('HeadGear', 'HeadGear:BP_Hat_BaseBall', 'Tan'),
+     ('Belt', 'Belt:BP_Battlebelt_TF', 'Black')],
     # 3
-    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Tan')],
+    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Tan'),
+     ('HeadGear', 'HeadGear:BP_Hat_BaseBall_Rev', 'Tan')],
     # 4
-    [('Shirt', 'Shirt:BP_Shirt_ButtonUp_Plain', 'Black')],
+    [('Shirt', 'Shirt:BP_Shirt_ButtonUp_Plain', 'Black'),
+     ('HeadGear', None, None),
+     ('Belt', 'Belt:BP_Battlebelt_TF', 'Black')],
     # 5
-    [('Shirt', 'Shirt:BP_Shirt_ButtonUp_Plain', 'Navy')],
+    [('Shirt', 'Shirt:BP_Shirt_ButtonUp_Plain', 'Navy'),
+     ('HeadGear', 'HeadGear:BP_Hat_BaseBall_Rev', 'Black'),
+     ('Belt', 'Belt:BP_Battlebelt_TF', 'Black')],
     # 6
-    [('Shirt', 'Shirt:BP_Shirt_Under', 'Red'),('Pants', 'Pants:BP_Pants_Jeans', 'Blue')],
+    [('Shirt', 'Shirt:BP_Shirt_Under', 'Red'),
+     ('Pants', 'Pants:BP_Pants_Jeans', 'Blue'),
+     ('HeadGear', 'HeadGear:BP_Hat_WatchCap', 'Tan'),
+     ('Belt', 'Belt:BP_Battlebelt_TF', 'Black')],
     # 7
-    [('Shirt', 'Shirt:BP_Shirt_Under', 'Navy'),('Pants', 'Pants:BP_Pants_Jeans', 'Blue')],
+    [('Shirt', 'Shirt:BP_Shirt_Under', 'Navy'),
+     ('Pants', 'Pants:BP_Pants_Jeans', 'Blue'),
+     ('HeadGear', 'HeadGear:BP_Hat_WatchCap', 'Black'),
+     ('Belt', 'Belt:BP_Battlebelt_TF', 'Black')],
     # 8
-    [('Shirt', 'Shirt:BP_Shirt_Under', 'Khaki'),('Pants', 'Pants:BP_Pants_Jeans', 'Blue')]
+    [('Shirt', 'Shirt:BP_Shirt_Under', 'Khaki'),
+     ('Pants', 'Pants:BP_Pants_Jeans', 'Blue'),
+     ('HeadGear', None, None),
+     ]
 ]
 
-VARIATIONS_FOR_HVT=[
+VARIATIONS_FOR_HVT = [
     # 1
     [('Shirt', 'Shirt:BP_Shirt_ButtonUp_Plain', 'Black')],
     # 2
-    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Tan')]
+    [('Shirt', 'Shirt:BP_Shirt_ButtonUp', 'Tan'), ('Belt', 'Belt:BP_Battlebelt_TF', 'Tan')]
 ]
 
 import json
+
 
 def load_kit(name):
     f = open(name)
@@ -37,32 +56,39 @@ def load_kit(name):
     f.close()
     return data
 
-def process_file(filename_prefix, variation_list):
+
+def process_file(filename, variation_list, special = None):
     i = 0
-    print("Processing " + filename_prefix)
+    print("Processing " + filename)
     for variation_list in variation_list:
         i = i + 1
-        data = load_kit(filename_prefix + '-template.json')
+        data = load_kit(filename + '-template.json')
 
         for (type, item, skin) in variation_list:
             for obj in data['Data']:
-                if obj['Type'] == 'Outfit':
+                if obj['Type'] == 'Outfit' or obj['Type'] == 'Gear':
                     for outfit_item in obj['Data']:
                         if outfit_item['Type'] == type:
-                            outfit_item['Item'] = item
-                            outfit_item['Skin'] = skin
+                            if item is None:
+                                obj['Data'].remove(outfit_item)
+                            else:
+                                outfit_item['Item'] = item
+                                outfit_item['Skin'] = skin
 
-        outfile_name = filename_prefix + str(i) + '.kit'
+        outfile_name = filename + str(i) + '.kit'
         print('Writing ' + outfile_name)
         with open(outfile_name, 'w', newline='\n') as outfile:
             outfile.write(json.dumps(data, indent='\t'))
             outfile.write("\n")
 
+
 def main():
-    prefix_list=['Narcos/Civ', 'Narcos/Tango_AR', 'Narcos/Tango_SMG', 'Narcos/Tango_SNP', 'Narcos/Tango_STG', 'Narcos/Tango_HDG']
+    prefix_list = ['Narcos/Civ', 'Narcos/Tango_AR', 'Narcos/Tango_SMG', 'Narcos/Tango_STG', 'Narcos/Tango_HDG']
     for filename_prefix in prefix_list:
         process_file('GroundBranch/Content/GroundBranch/AI/Loadouts/' + filename_prefix, VARIATIONS)
-    process_file('GroundBranch/Content/GroundBranch/AI/Loadouts/Narcos/HVT_AR', VARIATIONS_FOR_HVT)
+    process_file('GroundBranch/Content/GroundBranch/AI/Loadouts/Narcos/Tango_SNP', VARIATIONS, 'SNIPER')
+    process_file('GroundBranch/Content/GroundBranch/AI/Loadouts/Narcos/HVT_AR', VARIATIONS_FOR_HVT, 'HVT')
+
 
 if __name__ == "__main__":
     main()
